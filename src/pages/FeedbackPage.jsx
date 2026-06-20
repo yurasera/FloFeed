b91154389541b10c2f9c30a001bdd16b2a3b671c
yurasera <yuhaya.lissera@gmail.com>
2026-06-20 18:10:18 +0700

    fix: miss data path

--- src/pages/FeedbackPage.jsx
+++ src/pages/FeedbackPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { questions } from '../../data/questions';
+import { questions } from '../data/questions';
 
 const FeedbackPage = () => {
   const navigate = useNavigate();
