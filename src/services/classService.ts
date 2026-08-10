435efaa33ad3b825937153e430894ebdb99d0432
yurasera <yuhaya.lissera@gmail.com>
2026-08-11 04:24:07 +0700

    chore: takeour unuse services and data

--- src/services/classService.ts
+++
@@ -1,8 +0,0 @@
-import { mockClasses } from '../data/mockClasses'
-import type { Class } from '../types/feedback'
-
-export function findClassByCode(code: string): Class | null {
-    const normalizedCode = code.trim().toUpperCase()
-
-    return mockClasses.find((classItem) => classItem.code === normalizedCode) ?? null
-}
