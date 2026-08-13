a3777c6fea6faf4dad9c3978f15d1152f4a0374f
yurasera <yuhaya.lissera@gmail.com>
2026-08-14 00:06:22 +0700

    feat: reorganize components auth to features

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { LearnerAuthScreen } from '../components/LearnerAuthScreen'
+import { LearnerAuthScreen } from '../features/auth/components/LearnerAuthScreen'
 
 export function LoginPage() {
     return <LearnerAuthScreen mode="login" />
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import { LearnerAuthScreen } from '../components/LearnerAuthScreen'
+import { LearnerAuthScreen } from '../features/auth/components/LearnerAuthScreen'
 
 export function RegisterPage() {
     return <LearnerAuthScreen mode="register" />
